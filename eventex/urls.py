139798b2ccae3b02ba3422b000f7ee32f91d3986
fabriciogatto <fa_gatto7@yahoo.com.br>
2016-01-15 16:14:46 -0200

    Atualiza Admin page

--- eventex/urls.py
+++ eventex/urls.py
@@ -23,5 +23,5 @@ from eventex.subscriptions.views import subscribe
 urlpatterns = [
     url(r'^$', home),
     url(r'^inscricao/$', subscribe),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)), #login=admin senha=superadmin email=admin@admin.com
 ]
